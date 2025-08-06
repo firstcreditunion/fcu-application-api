87c7a226cc852487346d782ee0629ee9a9200108
Isaac Vicliph <admin@axiomspace.co.nz>
2025-08-06 14:15:55 +1200

    Added Sentry for Error Logs

---
+++ instrumentation-client.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a1af248591282f179b8b1d000e46839c@o4509794712158208.ingest.us.sentry.io/4509794717401088",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;

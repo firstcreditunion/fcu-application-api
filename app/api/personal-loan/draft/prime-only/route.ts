2d7acb3b64d4d5507bad33550b042152460dd8d4
Isaac Vicliph <admin@axiomspace.co.nz>
2025-08-06 14:43:21 +1200

    Added Route Handler for Prime

--- app/api/personal-loan/draft/prime-only/route.ts
+++ app/api/personal-loan/draft/prime-only/route.ts
@@ -1,16 +1,60 @@
 export async function POST(request: Request) {
   // Parse the request body
   const body = await request.json()
-  const { name } = body
+  const {
+    loanApplicationNumber,
+    primeClientNumber,
+    supabaseIntegrityState,
+    supabaseIntegrityJointState,
+
+    primePreliminaryQuestions,
+    primeDriversLicence,
+    primePassport,
+    primeFirearmsLicence,
+    primeBirthCertificate,
+    primeKiwiAccessCard,
+    primeCommunityServiceCard,
+    goldCard,
+    studentID,
+    primePersonalDetails,
+    primeEmployment,
+    primeContactDetails,
+    formFinancialDetails,
+    vehicleSecurity,
+    providentInsurance,
+  } = body
+
+  console.log(
+    'PRIME ONLY DRAFT APPLCATION DATA ',
+    loanApplicationNumber,
+    primeClientNumber,
+    supabaseIntegrityState,
+    supabaseIntegrityJointState,
+
+    primePreliminaryQuestions,
+    primeDriversLicence,
+    primePassport,
+    primeFirearmsLicence,
+    primeBirthCertificate,
+    primeKiwiAccessCard,
+    primeCommunityServiceCard,
+    goldCard,
+    studentID,
+    primePersonalDetails,
+    primeEmployment,
+    primeContactDetails,
+    formFinancialDetails,
+    vehicleSecurity,
+    providentInsurance
+  )
 
   // e.g. Insert new user into your DB
-  const newUser = { id: Date.now(), name }
+  const newUser = { id: Date.now() }
 
   return new Response(JSON.stringify(newUser), {
     status: 201,
     headers: {
       'Content-Type': 'application/json',
-      
     },
   })
 }
